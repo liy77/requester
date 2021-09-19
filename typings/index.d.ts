8db2c146d46c7c612e8b54ae5bccbeb250f58ab4
JustAWaifuHunter <73443391+JustAWaifuHunter@users.noreply.github.com>
2021-09-19 12:44:49 -0300

    Fix typing

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -10,3 +10,38 @@ declare interface HTTPSRequestOptions {
   body?: any;
   auth?: boolean;
 }
+
+declare interface HTTPSOptions {
+  version?: number;
+  requestTimeout?: number;
+  domain?: string;
+  headers: any;
+}
+
+declare interface HTTPSRequestOptions {
+  method?: string;
+  body?: any;
+  auth?: boolean;
+}
+
+declare module "requester" {
+  export = class Requester {
+    restVersion: number;
+    url: string;
+    domain: string;
+    timeout: number;
+    /**
+     * 
+     * @param token Client token for auth
+     * @param options HTTPS Options
+     */
+    constructor(token: string, options: HTTPSOptions)
+
+    /**
+     * Send request to discord api
+     * @param endpoint Endpoint for request
+     * @param options Request options
+     */
+    request(endpoint: string, options?: HTTPSRequestOptions): Promise<any>
+  }
+}
